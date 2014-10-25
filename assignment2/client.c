613a26c3413c134282fcbfa69b68ecfc0494b1e3
Sohil Gandhi <sogandhi@sbrocks.cewit.stonybrook.edu>
2014-10-25 06:52:26 -0400

    Local/Non Local

--- assignment2/client.c
+++ assignment2/client.c
@@ -9,7 +9,7 @@
 int checkLocal (struct clientStruct **cliInfo)
 {
         int isLocal;  
-        struct sockaddr_in sa;
+        struct sockaddr_in sa, *subnet;
         struct clientStruct *temp = *cliInfo;
         struct InterfaceInfo *head = temp->ifi_head;
         char src[128];
@@ -17,16 +17,17 @@ int checkLocal (struct clientStruct **cliInfo)
         inet_ntop(AF_INET, &temp->serv_addr.sin_addr, src, sizeof(src));
         if (strcmp(src, LOOPBACK) == 0)
         {
-                printf ("\nServer IP is Loopback Address. Client IP = 127.0.0.1");
+                printf ("\nServer IP is Loopback Address. Client IP = 127.0.0.1\n");
                 temp->cli_addr = temp->serv_addr;
                 return 1;
         }
-        /*
+        
         while (head)
         {
-                        
+		if ((temp->serv_addr.sin_addr.s_addr & head->ifi_ntmaddr.sin_addr.s_addr) == head->ifi_subnetaddr.sin_addr.s_addr)
+                        printf("\nFound match\n");
         }
-        */
+        
 
         return 0;
 
