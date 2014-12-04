461197ed213eac4a43bf3e2afc9bd1328b5b67d4
Shipra Gupta <shipgupta@sbrocks.cewit.stonybrook.edu>
2014-12-04 16:54:11 -0500

    First commit shipra

--- assignment4/utils.h
+++ assignment4/utils.h
@@ -2,5 +2,14 @@
 #define __UTILS_h
 
 #include    "unp.h"
+
+#define     UNIX_PATH     "unix_path"
+
+struct hwaddr{
+	int sll_ifindex;
+	unsigned short sll_hatype;
+	unsigned char sll_halen;
+	unsigned char sll_addr[8];
+};
 #endif  /* __UTILS_h */
 
